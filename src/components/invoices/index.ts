15c5cf7139eee1f1fdaa964f5ebcc036a8536643
saiotest <elyermad@gmail.com>
2026-02-08 00:18:08 -0300

    feat(SQ-28): add DueDatePicker component with presets and past date warning

--- src/components/invoices/index.ts
+++ src/components/invoices/index.ts
@@ -1,4 +1,5 @@
 export { ClientSelector } from './client-selector';
 export { CreateClientDialog } from './create-client-dialog';
+export { DueDatePicker } from './due-date-picker';
 export { TaxInput } from './tax-input';
 export { InvoiceSummary } from './invoice-summary';
